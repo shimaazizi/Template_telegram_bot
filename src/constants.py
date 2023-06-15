e6106c31157f753c49d6ea2213e33c4c942ea973
shima98 <shima.azizimajd98@gmail.com>
2023-06-14 23:24:38 -0700

    add base code for python telegram bot template

---
+++ src/constants.py
@@ -0,0 +1,11 @@
+from types import SimpleNamespace
+from src.utils.keyboard import create_keyboard
+
+keys = SimpleNamespace(
+    random_connect=':bust_in_silhouette: Random Connect',
+    settings=':gear: Settings'
+)
+
+keyboards = SimpleNamespace(
+    main=create_keyboard(keys.random_connect, keys.settings)
+)
